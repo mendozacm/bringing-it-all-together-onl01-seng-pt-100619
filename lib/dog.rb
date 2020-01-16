892ec87591ac2e0f3b0f36468913d7e1026e551d
Cesar Mendoza <mendoza.cesar92@gmail.com>
2020-01-16 04:47:28 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -9,7 +9,14 @@ class Dog
   end
   
   def self.create_table
-    
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS songs (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      album TEXT
+      )
+      SQL 
+    DB[:conn].execute(sql) 
   end
   
 end
