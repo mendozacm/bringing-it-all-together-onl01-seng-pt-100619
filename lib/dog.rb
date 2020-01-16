42e8f5cca2be678c4ecc2f023242ff1bde10b7fe
Cesar Mendoza <mendoza.cesar92@gmail.com>
2020-01-16 05:41:19 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -52,7 +52,18 @@ class Dog
     new_dog = self.new(attributes) 
   end
   
-  
+  def self.find_by_id(id)
+    sql = <<-SQL
+      SELECT *
+      FROM songs
+      WHERE id = ?
+      LIMIT 1
+    SQL
+ 
+    DB[:conn].execute(sql, id).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
   
   
 end
