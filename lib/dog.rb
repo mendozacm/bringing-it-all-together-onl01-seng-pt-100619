724d76c86841780767efc4dcbc518c68e67f2fe3
Cesar Mendoza <mendoza.cesar92@gmail.com>
2020-01-16 04:56:22 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -10,13 +10,13 @@ class Dog
   
   def self.create_table
     sql =  <<-SQL
-    CREATE TABLE IF NOT EXISTS songs (
-      id INTEGER PRIMARY KEY,
-      name TEXT,
-      album TEXT
-      )
-      SQL 
-    DB[:conn].execute(sql) 
+      CREATE TABLE IF NOT EXISTS songs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        album TEXT
+        )
+        SQL 
+      DB[:conn].execute(sql) 
   end
   
 end
